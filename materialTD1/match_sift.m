04083bf6cdffe5f09d0e06990ff69403892cc3e4
pedroperrusi <suruagyperrusi@etu.unistra.fr>
2019-03-26 18:47:53 +0100

    Begin of swift exercise

--- materialTD1/match_sift.m
+++ materialTD1/match_sift.m
@@ -1,4 +1,18 @@
 function [m1,m2] = match_sift(P1,P2,D1,D2)
-
-
-%... [TODO]
+    m1 = zeros(1, size(P1, 2));
+    diff1 = 10000 * ones(size(P1, 2)); % arbitrary large value
+    m2 = zeros(1, size(P2, 2));
+    diff2 = 10000 * ones(size(P1, 2)); % arbitrary large value
+    for i = 1 : size(P1, 2)
+        for j = 1 : size(P2, 2)
+            % compute the squared norm of each descriptor / histogram 
+            diffD = sum((D1(:, i) - D2(:, j)).*(D1(:, i) - D2(:, j)));
+            if(diffD < diff1(i))
+                m1(i) = j;
+            end
+            if(diffD < diff2(j))
+               m2(j) = i; 
+            end
+        end
+    end
+end
--- materialTD1/sift.m
+++ materialTD1/sift.m
@@ -8,9 +8,9 @@ I2 = imread('tps2.jpg');
 
 im1 = im2single(rgb2gray(I1)); 
 im2 = im2single(rgb2gray(I2)); 
-
-figure();imshow(im1);
-figure();imshow(im2);
+% 
+% figure();imshow(im1);
+% figure();imshow(im2);
 
 %% Compute SIFT features on images im1 and im2 using function vl_sift
 
@@ -21,10 +21,18 @@ SIFT_P1 = SIFT_F1(1:2,:);
 SIFT_P2 = SIFT_F2(1:2,:);
 
 
-
 %% Display features
+figure(); 
+imshow(im1);hold on;
+for i = 1 : size(SIFT_P1, 2)
+   plot(SIFT_P1(1, i), SIFT_P1(2, i), 'r+')
+end
 
-%... [TODO]
+figure(); 
+imshow(im2);hold on;
+for i = 1 : size(SIFT_P2, 2)
+   plot(SIFT_P2(1, i), SIFT_P2(2, i), 'g+')
+end
 
 
 %% Match features using function match_sift
@@ -33,8 +41,24 @@ SIFT_P2 = SIFT_F2(1:2,:);
 
 %% Display matched features
 
-%... [TODO]
+% Create a new side to side image
+rowsMatch = max(size(im1, 1), size(im2, 1));
+colsMatch = size(im1, 2) + size(im2, 2);
+ImageMatch = zeros(rowsMatch, colsMatch, 1);
 
+% add image 1
+ImageMatch(1 : size(im1, 1), 1 : size(im1, 2)) = im1;
+% add image 2 to its right side
+ImageMatch(1 : size(im2, 1), size(im1, 2) : colsMatch - 1) = im2;
+
+%... [TODO]
+figure();
+imshow(ImageMatch); hold on;
+for i = 1 : length(m1)
+   if(m1(i) ~= 0)
+       line(SIFT_P1(:, i)', [0 size(im1, 2)] + SIFT_P2(:, m1(i))')
+   end
+end
 
 
 %% Save matched features for later
